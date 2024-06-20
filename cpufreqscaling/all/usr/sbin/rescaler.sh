1242b7004861bada801f7bfaf22eced3f93c675d
AuxXxilium <info@auxxxilium.tech>
2024-06-20 22:00:44 +0200

    cpufreqscaling: fix
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- cpufreqscaling/all/usr/sbin/rescaler.sh
+++ cpufreqscaling/all/usr/sbin/rescaler.sh
@@ -13,7 +13,7 @@ cpucorecount=$(cat /proc/cpuinfo | grep processor | wc -l)
 cpucorecount=$((cpucorecount - 1))
 governor=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor)
 
-if [ -f "/usr/lib/modules/cpufreq_${1}" ] || [ "${1}" = "schedutil" ]; then
+if [ -f "/usr/lib/modules/cpufreq_${1}.ko" ] || [ "${1}" = "schedutil" ]; then
   # Set correct cpufreq governor to allow user defined frequency scaling
   if [ "$governor" != "${1}" ]; then
     for i in $(seq 0 "${cpucorecount}"); do
