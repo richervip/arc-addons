8770444fd786b3c18967b3913dc8d630800476ea
AuxXxilium <info@auxxxilium.tech>
2024-08-02 18:11:02 +0200

    remotefs: add libmediaindex
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- remotefs/install.sh
+++ remotefs/install.sh
@@ -22,10 +22,27 @@ if [ "${1}" = "late" ]; then
   else
     echo "libsynosdk.so.7 not found"
   fi
+  SO_FILE="/tmpRoot/usr/lib/libmediaindex.so"
+  if [ -f "${SO_FILE}" ]; then
+    if [ ! -f "${SO_FILE}.bak" ]; then
+      echo "Backup ${SO_FILE}"
+      cp -vfp "${SO_FILE}" "${SO_FILE}.bak"
+    fi
+    echo "Patching libmediaindex.so"
+    PatchELFSharp "${SO_FILE}" "IndexIsPathRemoteMount" "B8 00 00 00 00 C3"
+    PatchELFSharp "${SO_FILE}" "IndexIsPathRemoteOrImageMount" "B8 00 00 00 00 C3"
+  else
+    echo "libmediaindex.so not found"
+  fi
 elif [ "${1}" = "uninstall" ]; then
   echo "Installing addon remotefs - ${1}"
 
   SO_FILE="/tmpRoot/usr/lib/libsynosdk.so.7"
+  if [ -f "${SO_FILE}.bak" ]; then
+    echo "Restore ${SO_FILE}"
+    mv -f "${SO_FILE}.bak" "${SO_FILE}"
+  fi
+  SO_FILE="/tmpRoot/usr/lib/libmediaindex.so"
   if [ -f "${SO_FILE}.bak" ]; then
     echo "Restore ${SO_FILE}"
     mv -f "${SO_FILE}.bak" "${SO_FILE}"
