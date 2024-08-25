d44a42db3406e24bd8081d724d39b0bc7c76f37a
AuxXxilium <info@auxxxilium.tech>
2024-08-25 16:55:44 +0200

    cpufreqscaling: fix
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- cpufreqscaling/install.sh
+++ cpufreqscaling/install.sh
@@ -11,15 +11,13 @@ if [ "${1}" = "late" ]; then
   mkdir -p "/tmpRoot/usr/arc/addons/"
   cp -vf "${0}" "/tmpRoot/usr/arc/addons/"
 
-  cp -vf /usr/bin/scaler.sh /tmpRoot/usr/bin/scaler.sh
-  cp -vf /usr/bin/unscaler.sh /tmpRoot/usr/bin/unscaler.sh
-  cp -vf /usr/bin/rescaler.sh /tmpRoot/usr/bin/rescaler.sh
-  [ ! -f "/tmpRoot/usr/bin/echo" ] && cp -vf /usr/bin/echo /tmpRoot/usr/bin/echo
-
-  if [ "${2}" = "userspace" ]; then
-    mkdir -p "/tmpRoot/usr/lib/systemd/system"
-    DEST="/tmpRoot/usr/lib/systemd/system/cpufreqscaling.service"
-    cat << EOF > ${DEST}
+  cp -vf /usr/sbin/scaling.sh /tmpRoot/usr/sbin/scaling.sh
+  [ ! -f "/tmpRoot/usr/bin/echo" ] && cp -vf /usr/bin/echo /tmpRoot/usr/bin/echo || true
+  cp -f /usr/lib/modules/acpi_cpufreq.ko /tmpRoot/usr/lib/modules/acpi_cpufreq.ko
+  [ "${2}" != "schedutil" ] && cp -vf /usr/lib/modules/cpufreq_${2}.ko /tmpRoot/usr/lib/modules/cpufreq_${2}.ko && modprobe cpufreq_${2} || true
+  mkdir -p "/tmpRoot/usr/lib/systemd/system"
+  DEST="/tmpRoot/usr/lib/systemd/system/cpufreqscaling.service"
+  cat << EOF > ${DEST}
 [Unit]
 Description=Enable CPU Freq scaling
 DefaultDependencies=no
@@ -31,8 +29,8 @@ User=root
 Type=simple
 Restart=on-failure
 RestartSec=10
-ExecStartPre=sleep 30
-ExecStart=/usr/bin/scaler.sh
+ExecStartPre=sleep 10
+ExecStart=/usr/sbin/scaling.sh "${2}"
 
 [Install]
 WantedBy=multi-user.target
@@ -42,40 +40,11 @@ Author=Virtualization Team
 EOF
     mkdir -vp /tmpRoot/usr/lib/systemd/system/multi-user.target.wants
     ln -vsf /usr/lib/systemd/system/cpufreqscaling.service /tmpRoot/usr/lib/systemd/system/multi-user.target.wants/cpufreqscaling.service
-  else
-    [ "${2}" != "schedutil" ] && cp -vf /usr/lib/modules/cpufreq_${2}.ko /tmpRoot/usr/lib/modules/cpufreq_${2}.ko && modprobe cpufreq_${2}
-    mkdir -p "/tmpRoot/usr/lib/systemd/system"
-    DEST="/tmpRoot/usr/lib/systemd/system/cpufreqscaling.service"
-    cat << EOF > ${DEST}
-[Unit]
-Description=Enable CPU Freq scaling
-DefaultDependencies=no
-IgnoreOnIsolate=true
-After=udevrules.service
-
-[Service]
-User=root
-Type=simple
-Restart=on-failure
-RestartSec=10
-ExecStart=/usr/bin/rescaler.sh "${2}"
-
-[Install]
-WantedBy=multi-user.target
-
-[X-Synology]
-Author=Virtualization Team
-EOF
-    mkdir -vp /tmpRoot/usr/lib/systemd/system/multi-user.target.wants
-    ln -vsf /usr/lib/systemd/system/cpufreqscaling.service /tmpRoot/usr/lib/systemd/system/multi-user.target.wants/cpufreqscaling.service
-  fi
 elif [ "${1}" = "uninstall" ]; then
   echo "Installing cpufreqscalingscaling - ${1}"
 
   rm -f "/tmpRoot/usr/lib/systemd/system/multi-user.target.wants/cpufreqscaling.service"
   rm -f "/tmpRoot/usr/lib/systemd/system/cpufreqscaling.service"
 
-  rm -f /tmpRoot/usr/bin/scaler.sh
-  rm -f /tmpRoot/usr/bin/rescaler.sh
-  rm -f /tmpRoot/usr/bin/unscaler.sh
+  rm -f /tmpRoot/usr/sbin/scaling.sh
 fi
